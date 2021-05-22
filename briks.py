03a61e259272121fb584ea2df7997bdbfa4ca342
levon <airtk@ya.ru>
2021-05-22 19:03:14 +0300

    Класс новый проект уууууууууууухухухухухухухухху

--- briks.py
+++ briks.py
@@ -2,11 +2,17 @@ import pygame
 pygame.init()
 pygame.font.init()
 f=pygame.font.get_default_font()
-shrift=pygame.font.SysFont(f,50)
+shrift=pygame.font.SysFont("arial",50)
 def kub_poyavis(okno,kub,hp_kuba):
 
     pygame.draw.rect(okno, [0,39,255],kub,0,15)
     pygame.draw.rect(okno, [233, 20, 0], kub,5,15)
-    hp_kuba = shrift.render(str(hp_kuba), True, [255, 2, 10])
-    okno.blit(hp_kuba,[380, 580,20,20])
+    hp_kuba = shrift.render(str(hp_kuba), True, [0, 255, 10])
+
+    kpd = pygame.Rect(0,0,hp_kuba.get_width(),hp_kuba.get_height() )
+    kpd.center=kub.center
+    okno.blit(hp_kuba,[kpd.x,kpd.y])
+
+    # pygame.draw.rect(okno, [233, 20, 0], kpd,5,15)
+
     # okno.blit(kub,[100,55,173])
--- mayn.py
+++ mayn.py
@@ -1,11 +1,12 @@
 import  pygame,briks
 from pygame import display, event
 
-kub = pygame.Rect(333, 555, 100, 100)
+kub = pygame.Rect(633, 555, 100, 100)
+okno = display.set_mode([750, 825])
 
 hp_kuba=2
 while True :
-    okno= display.set_mode([750, 825])
+    pygame.event.get()
+    okno.fill([0,0,0])
     briks.kub_poyavis(okno,kub,hp_kuba)
-
     pygame.display.flip ()
