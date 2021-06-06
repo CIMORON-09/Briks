8f8cf5b5582ef3d90c1729817979b273e5460c25
levon <airtk@ya.ru>
2021-06-06 12:25:31 +0300

    Класс новый проект не уууууууууууухухухухухухухухху??!!

--- mayn.py
+++ mayn.py
@@ -2,31 +2,13 @@ import  pygame,briks,time
 from pygame import display, event
 okno = display.set_mode([750, 820])
 spisok_kubika=[]
-gggg=[100,200,300]
+gggg=[100,200,300,400,500,600]
 
 for did in gggg:
     kub1 = pygame.Rect(0,did, 100, 100)
     kub1.centerx = okno.get_width() / 2
     spisok_kubika.append(kub1)
-    # kub2 = pygame.Rect(0, 200, 100, 100)
-    # kub2.centerx = okno.get_width() / 2
-    # spisok_kubika.append(kub2)
-    #
-    # kub3 = pygame.Rect(0, 300, 100, 100)
-    # kub3.centerx = okno.get_width() / 2
-    # spisok_kubika.append(kub3)
 
-    # kub4 = pygame.Rect(0, 400, 100, 100)
-    # kub4.centerx = okno.get_width() / 2
-    # spisok_kubika.append(kub4)
-    #
-    # kub5 = pygame.Rect(0, 500, 100, 100)
-    # kub5.centerx = okno.get_width() / 2
-    # spisok_kubika.append(kub5)
-    #
-    # kub6 = pygame.Rect(0, 600, 100, 100)
-    # kub6.centerx = okno.get_width() / 2
-    # spisok_kubika.append(kub6)
 
     print(did)
 
