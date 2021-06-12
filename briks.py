403f2e331d798a0b8c7095570806d0d325071e80
levon <airtk@ya.ru>
2021-06-12 19:04:35 +0300

    Kos сегодня все позвоните прошу хорошо все получилось сделали дофига кубиков щас наверно пойду с мамой делать новый аккаунт стиме потому что CAS гоу не запускается

--- briks.py
+++ briks.py
@@ -3,16 +3,17 @@ pygame.init()
 pygame.font.init()
 f=pygame.font.get_default_font()
 shrift=pygame.font.SysFont("arial",50)
-def kub_poyavis(okno,kub,hp_kuba):
+def kub_poyavis(okno,kub):
     fon = pygame.image.load("fon.jpeg")
 
-    pygame.draw.rect(okno, [0,39,255],kub,0,15)
-    pygame.draw.rect(okno, [233, 20, 0], kub,5,15)
-    hp_kuba = shrift.render(str(hp_kuba), True, [0, 255, 10])
+    pygame.draw.rect(okno, [0,39,255],kub["rect"],0,15)
+    pygame.draw.rect(okno, [233, 20, 0], kub["rect"],5,15)
+    hp_kuba = shrift.render(str(kub["hp"]), True, [0, 255, 10])
+
 
 
     kpd = pygame.Rect(0,0,hp_kuba.get_width(),hp_kuba.get_height() )
-    kpd.center=kub.center
+    kpd.center=kub["rect"].center
     okno.blit(hp_kuba,[kpd.x,kpd.y])
 
     # pygame.draw.rect(okno, [233, 20, 0], kpd,5,15)
--- mayn.py
+++ mayn.py
@@ -1,4 +1,4 @@
-import  pygame,briks,time
+import  pygame,briks,time,random
 from pygame import display, event
 okno = display.set_mode([700, 800])
 fon = pygame.image.load("fon.jpeg")
@@ -6,20 +6,23 @@ fon = pygame.image.load("fon.jpeg")
 spisok_kubika=[]
 y2=[100,200,300,400,500,600]
 x2=[150,250,350,450]
-0
+
+
 for x1 in x2:
 
 
     for y1 in y2:
         kub1 = pygame.Rect(x1,y1, 100, 100)
-        spisok_kubika.append(kub1)
+        foto = random.randint(1, 100)
+        kub0={"rect":kub1,"hp":foto}
+        spisok_kubika.append(kub0)
 
 
 
 
 
 # ДЕНЬ РОЖДЕНИЯ У МОЕГО УЧИТЕЛЯ 4 СЕНТЯБРЯ!!(cool) 
-hp_kuba=2
+
 while True :
 
     pygame.event.get()
@@ -30,7 +33,7 @@ while True :
     # time.sleep(1)
     for did in spisok_kubika:
 
-        briks.kub_poyavis(okno,did, hp_kuba)
+        briks.kub_poyavis(okno,did)
         # pygame.display.flip()
         # time.sleep(1)
     pygame.display.flip ()
