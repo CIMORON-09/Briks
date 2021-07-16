ccba75a22dac8fb6ee29f137ae39a455e008b4d0
levon <airtk@ya.ru>
2021-07-16 13:09:35 +0300

    grsrts

--- ball.py
+++ ball.py
@@ -1,16 +1,29 @@
-import  pygame
-x=10
-y=300
-balle = pygame.Rect(x, y,30,30)
+import pygame
+
+x = 10
+y = 300
+speedx = 2
+speedy = 2
+balle = pygame.Rect(x, y, 30, 30)
 
 
 def risovanie_ball(okno):
-        pygame.draw.circle(okno,[250,250,250],[balle.x,balle.y],15)
-
+    pygame.draw.circle(okno, [250, 250, 250], [balle.centerx, balle.centery], 15)
+    # pygame.draw.rect(okno,[2,25,250],balle,5)
 
 def slezhu_za_granicami():
+    global speedx, speedy
+    if balle.right > 700:
+        speedx =-2
 
-    if balle.x>685:
-        balle.x-=1
+    if balle.left < 0:
+        speedx = +2
+    print(speedx)
+
+    if balle.y < 0:
+        speedy =+2
+
+    if balle.bottom >800:
+        speedy =-2
 
 # 😁😁😁😆😁😊🍎😳😳🤬🤬🥸🥸
--- mayn.py
+++ mayn.py
@@ -1,44 +1,35 @@
-import pygame,briks,ball
+import pygame, briks, ball
 from pygame import display
+
 okno = display.set_mode([700, 800])
 
-
-
-
 fon = pygame.image.load("fon.png")
-p=fon.get_height()/fon.get_width()
-fon = pygame.transform.scale(fon, [700,int(700*p)])
-spisok_kubika=[]
-y2=[100,200,300,400,500,600]
-x2=[150,250,350,450]
-
+p = fon.get_height() / fon.get_width()
+fon = pygame.transform.scale(fon, [700, int(700 * p)])
+spisok_kubika = []
+y2 = [100, 200, 300, 400, 500, 600]
+x2 = [150, 250, 350, 450]
 
 for x1 in x2:
 
-
     for y1 in y2:
-        kub1 = pygame.Rect(x1,y1, 100, 100)
+        kub1 = pygame.Rect(x1, y1, 100, 100)
 
-        kub0={"rect":kub1,"hp":1}
+        kub0 = {"rect": kub1, "hp": 1}
         spisok_kubika.append(kub0)
-
-
-
-
 
 # ДЕНЬ РОЖДЕНИЯ У МОЕГО УЧИТЕЛЯ 4 СЕНТЯБРЯ!!(cool) 
 
-while True :
+while True:
     pygame.event.get()
-    okno.fill([0,0,0])
+    okno.fill([0, 0, 0])
     okno.blit(fon, [0, 0])
-    ball.balle.x+=1
+    ball.balle.x += ball.speedx
+    ball.balle.y += ball.speedy
 
-    # for did in spisok_kubika:
-    #
-    #     briks.kub_poyavis(okno,did)
+    for did in spisok_kubika:
+        briks.kub_poyavis(okno,did)
 
     ball.risovanie_ball(okno)
     ball.slezhu_za_granicami()
     pygame.display.flip()
-
