bf128927bac4c9c385bf56ba2b27024d943de901
levon <airtk@ya.ru>
2021-07-23 13:30:13 +0300

    grsrts

--- controller.py
+++ controller.py
@@ -2,10 +2,14 @@ import pygame
 
 def obrabotk_sobity():
     spisok=pygame.event.get()
-    werty=len(spisok)
+    # werty=len(spisok)
+    for did in spisok:
+        if did.type==pygame.MOUSEBUTTONDOWN:
+            print(did.pos[0],did.pos[1])
+    # if werty!=0:
+    #     # print(spisok)
+    #     print(spisok[0])
 
-    if werty!=0:
-        # print(spisok)
-        print(spisok[0])
-    # if MouseMotionKeyDown:
-    #     print("нажал")
+    # for did in werty:
+    #     if did.type == pygame.MOUSEBUTTONDOWN:
+    #         print("нажал")
