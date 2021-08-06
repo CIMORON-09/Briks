46377782e431a41ca4c623493d945e97736c4941
levon <airtk@ya.ru>
2021-08-06 13:34:29 +0300

    grsrts

---
+++ efaef.py
@@ -0,0 +1,24 @@
+Black_Prins = 44
+Maus = []
+Maus.append(23)
+
+E75 = Black_Prins
+
+Black_Prins = 5
+E75 = Black_Prins
+
+E75 = Maus
+Maus.append(233)
+E75[1] = 777
+Yoh = [1, 4, 9]
+E75.append(Yoh)
+Yoh[1] = 444
+Yoh = 0
+E75[2][1] = 34
+Badger_Esq = {"Tank": "Badger_Esq", "Bb": 294, "Gold": 388, "Of": 68}
+Badger_Esq["Of"] = 78
+
+Tanki = [Badger_Esq]
+Badger_Esq = {"Tank": "Badger_Esq", "Bb": 2494, "Gold": 3488, "Of": 64448}
+Tanki.append(Badger_Esq)
+Badger_Esq = 0
